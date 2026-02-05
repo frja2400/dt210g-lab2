56c30a8f0ae5cc84c19eeb3c5ef5d44ea5152df1
Frida Jansson <fridahannajansson@gmail.com>
2026-02-05 10:51:11 +0100

    Uppdaterar mapp- och filnamn på mitt interface och uppdaterar todoApi.ts med asynkrona funktioner som uppdaterar, skapar och raderar todos med fetch-anrop.

--- src/services/todoApi.ts
+++ src/services/todoApi.ts
@@ -1,4 +1,5 @@
-import type { Todo } from "../types/Todo";
+// Importerar Todo-typen från TodoInterface.ts
+import type { Todo } from "../interfaces/TodoInterface";
 
 // Url till mitt API
 const API_URL = "http://159.223.216.135/api/todo";
@@ -13,4 +14,38 @@ export const getTodos = async (): Promise<Todo[]> => {
   }
 
   return response.json();
+};
+
+// Skapa ny todo (plocka bort id från Todo-typen eftersom det genereras av servern)
+export const createTodo = async (todo: Omit<Todo, "id">): Promise<Todo> => {
+  const response = await fetch(API_URL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(todo),
+  });
+  if (!response.ok) throw new Error("Failed to create todo");
+  return response.json();
+};
+
+// Uppdatera todo (skicka hela objektet inklusive id)
+export const updateTodo = async (id: number, todo: Todo): Promise<Todo> => {
+  const response = await fetch(`${API_URL}/${id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(todo),
+  });
+  
+  if (!response.ok) {
+    throw new Error("Failed to update todo");
+  }
+
+  return todo;
+};
+
+// Ta bort todo med givet id och returnerar en Promise som inte returnerar något värde
+export const deleteTodo = async (id: number): Promise<void> => {
+  const response = await fetch(`${API_URL}/${id}`, {
+    method: "DELETE",
+  });
+  if (!response.ok) throw new Error("Failed to delete todo");
 };
